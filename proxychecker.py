49fe93ce913f6ad5c5f7e668bca33e511b050151
Joey <112933254+aspiirant@users.noreply.github.com>
2023-10-12 13:05:33 +0200

    Update proxychecker.py

--- proxychecker.py
+++ proxychecker.py
@@ -1,32 +1,59 @@
 import requests
 
+# function to check if a proxy is working
 def check_proxy(proxy):
     try:
-        response = requests.get("https://www.example.com", proxies={"http": proxy, "https": proxy}, timeout=10)
+        response = requests.get("https://www.example.com", proxies={"http": proxy, "https": proxy}, timeout=5)
         if response.status_code == 200:
             return True
     except Exception as e:
-        pass
+        print(f"Exception occurred: {e}")
     return False
 
+# main function
 def main():
-    # Fill the list with your own proxy sites
-    proxy_list = [
-        "http://proxy1.example.com:8080",
-        "http://proxy2.example.com:8080",
-        "http://proxy3.example.com:8080",
-        # Add more proxy servers as needed
-    ]
+    print("Thank you for using my tool!")
 
-    available_proxies = []
+    while True:
+        command = input("Type check to begin (exit to stop): ")
 
-    for proxy in proxy_list:
-        if check_proxy(proxy):
-            available_proxies.append(proxy)
+        if command == "":
+            print("No command was entered! Please try again.")
+            break
 
-    # Write the available proxies to a text file
-    with open("available_proxies.txt", "w") as file:
-        file.write("\n".join(available_proxies))
+        if command == "exit":
+            print("Quitting..")
+            break
+
+        elif command == "check":
+            file_path = input("Enter the path to the proxy file: ")
+
+            # read the proxies from the file
+            with open(file_path, 'r') as file:
+                proxies = file.read().splitlines()
+
+            available_proxies = []
+
+            # check if the proxies are working
+            for proxy in proxies:
+                if check_proxy(proxy):
+                    print(f"Proxy {proxy} is working!")
+                    available_proxies.append(proxy)
+                else:
+                    print(f"Proxy {proxy} is not working!")
+
+            # select your output file_path
+            output_file_path = input("Enter the path to the output file: ")
+
+            # write available proxies to that file_path
+            with open(output_file_path, 'w') as file:
+                for proxy in available_proxies:
+                    file.write(proxy + "\n")
+
+            if available_proxies:
+                print(f"Succesfully printed all the available proxies to {output_file_path}")
+            else:
+                print("No proxies were available!")
 
 if __name__ == "__main__":
     main()
